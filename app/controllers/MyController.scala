caadbadedbdb3cf0d8617f9a1a07a3ef1c93b7c3
Nine <max.k.saito@gmail.com>
2015-11-23 22:33:36 +0900

    first commit

---
+++ app/controllers/MyController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created on 2015/11/23.
+ */
+class MyController extends Controller {
+
+  def index = Action {
+    Ok(views.html.sample("Your new application is ready."))
+  }
+
+}
