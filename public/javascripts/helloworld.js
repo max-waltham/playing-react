7aad9cdfa20df6c992a92ec73d1f6f9d9188ab68
Kota Saito <max.k.saito@gmail.com>
2015-11-25 19:21:46 +0900

    js 改変

--- public/javascripts/helloworld.js
+++ public/javascripts/helloworld.js
@@ -3,12 +3,38 @@ ReactDOM.render(
   document.getElementById('c1')
 );
 
+
+
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 // tutorial1.js
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
@@ -18,3 +44,4 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('c2')
 );
+
