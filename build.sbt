8d99abceb89d7550b2cf4bbc9f3ab4930c28a2ba
Nine <max.k.saito@gmail.com>
2015-11-27 01:13:21 +0900

    flat?

--- build.sbt
+++ build.sbt
@@ -9,20 +9,13 @@ scalaVersion := "2.11.6"
 libraryDependencies ++= Seq(jdbc, cache, ws, specs2 % Test)
 
 libraryDependencies ++= Seq(
-  //"com.typesafe.play" %% "anorm" % "2.4.0",
-
-  // "com.typesafe.play" %% "play-slick" % "1.1.0",
-  // "com.typesafe.play" %% "play-slick-evolutions" % "1.1.0",
-  // "mysql" % "mysql-connector-java" % "5.1.36",
 
   "org.webjars" %% "webjars-play" % "2.4.0-1",
-  // "org.webjars.npm" % "bootstrap" % "3.3.6",
+  "org.webjars" % "jquery" % "2.1.4",
   "org.webjars.bower" % "adminlte" % "2.3.2",
   "org.webjars" % "flat-ui" % "bcaf2de95e",
 
   "org.webjars.bower" % "react" % "0.14.3"
-  // "org.webjars.npm" % "react-dom" % "0.14.2"
-  // "org.webjars" % "marked" % "0.3.2"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
--- public/javascripts/helloworld.js
+++ public/javascripts/helloworld.js
@@ -4,7 +4,7 @@ ReactDOM.render(
 );
 
 // tutorial2.js
-var CommentList = React.createClass({
+let CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
@@ -14,18 +14,19 @@ var CommentList = React.createClass({
   }
 });
 
-var CommentForm = React.createClass({
+let CommentForm = React.createClass({
   render: function() {
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+        {this.props.key}
       </div>
     );
   }
 });
 
 // tutorial1.js
-var CommentBox = React.createClass({
+let CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
@@ -33,7 +34,7 @@ var CommentBox = React.createClass({
         <h1>Comments</h1>
         <CommentList />
         {
-          [1,2,3].map((a) => {
+          [1,2,3].map(a => {
             return <CommentForm key={a}/>;
           })
         }
@@ -43,8 +44,24 @@ var CommentBox = React.createClass({
   }
 });
 
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('c2')
-);
+let ExampleBox = React.createClass({
+  render: function() {
+    return (
+    <div className="box">
+    <div className="box-header with-border">
+      <h3 class="box-title">Title</h3>
+    </div>
+
+    <div className="box-body">
+      <a href='/assets/lib/adminlte/index.html' target="_blank">AdminLTE Example</a>
+    </div>
+    <div className="box-body">
+      <a href='/assets/lib/adminlte/index2.html' target="_blank">AdminLTE Example2</a>
+    </div>
+    </div>
+    );
+  }
+});
+
+ReactDOM.render( <ExampleBox />, document.getElementById('c2'));
 
