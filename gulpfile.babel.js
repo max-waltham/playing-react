9bb9ea28c4c23943297d48d5367ada2532ff68ac
Nine <max.k.saito@gmail.com>
2015-12-01 23:08:45 +0900

    完全に動作するサンプル

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ gulp.task('babel', function () {
   return browserify('./public/javascripts/app.js', { debug: true })
     .transform(babelify, {presets: ["es2015", "react"]})
     .bundle()
-    .pipe(plumber()) // エラー時にWatchを停止しないためのモジュール
+    .pipe(plumber()) // エラー時にWatchを停止しないためのモジュール 止まるけど
     .pipe((source)('all.js'))
     .pipe(gulp.dest('./target/web/public/main/dist'));
 });
